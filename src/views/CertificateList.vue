7c13fe4c053172613be70a1dd04211f34e732dfe
Leonardo <leonardo@codebit.com.br>
2019-03-22 13:07:41 -0300

    #1 Single File Components

--- src/views/CertificateList.vue
+++ src/views/CertificateList.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>Certificates Listing</h1>
+    <CertificateCard/>
+
     <router-link :to="{ name: 'certificate-show', params: { id: 1 } }">Show Certificate #1</router-link>|
   </div>
 </template>
+
+<script>
+import CertificateCard from '@/components/CertificateCard.vue'
+
+export default {
+  components: {
+    CertificateCard
+  }
+}
+</script>
+
+<style scoped>
+</style>
