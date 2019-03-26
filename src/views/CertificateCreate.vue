da1096f25dd5de7cb0f5f3957f0e032d522e5ac5
Leonardo <leonardo@codebit.com.br>
2019-03-26 17:08:43 -0300

    Starting use Store

--- src/views/CertificateCreate.vue
+++ src/views/CertificateCreate.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Create Certificate</h1>
+  <div>
+    <h1>Create an Event, {{ userName }}</h1>
+    <p>This is user ID {{ userID }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    },
+    userID() {
+      return this.$store.state.user.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
