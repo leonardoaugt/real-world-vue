131265064a9c2836696fecd376790a4b8913442d
Leonardo <leonardo@codebit.com.br>
2019-03-26 10:29:36 -0300

    Named Slot

--- src/components/CertificateCard.vue
+++ src/components/CertificateCard.vue
@@ -4,7 +4,9 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
+      <BaseIcon name="users">
+        <span slot="description">{{ event.attendees.length }} attending</span>
+      </BaseIcon>
     </div>
   </router-link>
 </template>
